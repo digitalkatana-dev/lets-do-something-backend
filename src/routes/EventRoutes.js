f0571c5213f20ee231d0c32e3ba93fa0895a8c22
Brandon Benoit <b.nutty@gmail.com>
2023-10-13 11:03:23 -0700

    updated urls again

--- src/routes/EventRoutes.js
+++ src/routes/EventRoutes.js
@@ -76,7 +76,7 @@ router.post('/events', requireAuth, async (req, res) => {
 						req?.body?.date
 					} at ${dayjs(req?.body?.time).format('h:mm a')} by ${
 						req?.user?.firstName
-					}. Click here -> http://localhost:3000 to RSVP!`,
+					}. Click here -> https://letsdosomething.net/ to RSVP!`,
 					from: process.env.TWILIO_NUMBER,
 					to: `+1${item.phone}`,
 				});
@@ -89,7 +89,7 @@ router.post('/events', requireAuth, async (req, res) => {
 						<h4>You've been invited to ${req?.body?.type} on ${req?.body?.date} at ${dayjs(
 						req?.body?.time
 					).format('h:mm a')} by ${req?.user?.firstName}.</h4>
-						<h5>Click <a href="http://localhost:3000" style={{textDecoration: none}}>here</a> to RSVP!</h5>
+						<h5>Click <a href="https://letsdosomething.net/" style={{textDecoration: none}}>here</a> to RSVP!</h5>
 					</div>`,
 				};
 
@@ -191,7 +191,7 @@ router.post('/events/invite', requireAuth, async (req, res) => {
 					time
 				).format('h:mm a')} by ${
 					req?.user?.firstName
-				}. Click here -> http://localhost:3000 to RSVP!`,
+				}. Click here -> https://letsdosomething.net/ to RSVP!`,
 				from: process.env.TWILIO_NUMBER,
 				to: `+1${guest.phone}`,
 			});
@@ -204,7 +204,7 @@ router.post('/events/invite', requireAuth, async (req, res) => {
 						<h4>You've been invited to ${type} on ${date} at ${dayjs(time).format(
 					'h:mm a'
 				)} by ${req?.user?.firstName}.</h4>
-						<h5>Click <a href="http://localhost:3000" style={{textDecoration: none}}>here</a> to RSVP!</h5>
+						<h5>Click <a href="https://letsdosomething.net/" style={{textDecoration: none}}>here</a> to RSVP!</h5>
 					</div>`,
 			};
 
